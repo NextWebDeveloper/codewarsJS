b101267ceac704d6bb3851479da9ab0780830230
Тощев Дмитрий Владимирович <toshchevdv@site-master.org>
2018-07-30 14:59:27 +0300

    initial commit

---
+++ app/js/common.js
@@ -0,0 +1,83 @@
+/* 
+// Гласные буквы в строке
+
+let x = getCount('aaaaaiou');
+console.log(x);
+
+function getCount(str) {
+  var vowelsCount = 0;
+  var vowels = 'aeiou';
+  for (i=0; i<str.length; i++) {
+    for (j=0; j<vowels.length; j++) {
+      if (str[i] == vowels[j]) {
+        vowelsCount += 1;
+      }
+    }
+  }
+  return vowelsCount;
+} */
+
+
+/* 
+// Повторить строку n раз
+
+let x = repeatStr(1, 'abc');
+console.log(x); 
+
+function repeatStr (n, s) {
+  var repeatedStr = '';
+  for (i=0; i<n; i++) {
+    repeatedStr += s;
+  }
+  return repeatedStr;
+} */
+
+/* 
+// Вернуть максимум и минимум из строки чисел
+// highAndLow("1 22 -3 4 5"); // return "5 1"
+
+function highAndLow(numbers){
+	let convertedToNumbers = numbers.split(' ');
+	let max = +convertedToNumbers[0];
+	let min = +convertedToNumbers[0];
+	for (i=0; i<convertedToNumbers.length; i++) {
+		convertedToNumbers[i] = +convertedToNumbers[i];
+		if (convertedToNumbers[i] > max) max = convertedToNumbers[i];
+		if (convertedToNumbers[i] < min) min = convertedToNumbers[i];
+	}
+	const maxAndMin = max + ' ' + min;
+	// return maxAndMin;
+	console.log(maxAndMin);
+} */
+
+
+// Сумма всех чисел от 1 до числа в аргументе функции
+// summation(8) -> 36  
+// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+/* console.log(summation(4));
+
+const summation = function (num) {
+	let sum = 0;
+	for (i=1; i < (num+1); i++ ) {
+		sum += i;
+	}
+	console.log(sum);
+} */
+ 
+
+/* var numbers = [1, 2, 2, 0, -5, 10];
+var sum = 0;
+numbers.forEach(
+	function sumNumber( xi ) {
+		sum += xi;
+  }
+);
+console.log(sum); */
+
+
+var x;
+
+true || (x = 1);
+
+alert(x);
